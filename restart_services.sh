17994f999bf2e3436d164ed8dbfbdfb045f4494b
bioinfo <bioinfo@example.com>
2019-03-27 10:35:45 +0100

    [CHANGE] working directory

--- restart_services.sh
+++ restart_services.sh
@@ -1,2 +1,2 @@
 docker stop flask_app && docker rm flask_app
-docker build -t flask-sample-one:latest . && docker run -d --name flask_app -v /Users/wallace/Documents/DOCKER/myproject:/app -p 5000:5000 flask-sample-one
+docker build -t flask-sample-one:latest . && docker run -d --name flask_app -v ${PWD}:/app -p 5000:5000 flask-sample-one
